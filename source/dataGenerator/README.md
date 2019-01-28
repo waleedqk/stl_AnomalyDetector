18045802959358ade214b8828f535482bc53d35f
Waleed Khan <wqkhan@uwaterloo.ca>
2019-01-28 11:08:45 -0500

    Generating data files with signal values for testing

---
+++ source/dataGenerator/README.md
@@ -0,0 +1,3 @@
+# README
+
+Jupyter notebook that generates a CSV file that has different signals to use for testing the stlgrammar code.
---
+++ source/dataGenerator/commit_git.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENT_DIR=$(dirname "$SCRIPT_DIR")
+
+
+FILE_LIST=(
+    commit_git.bash
+    README.md
+    dataGen.ipynb
+    data_Signals.csv
+)
+
+
+git add $SCRIPT_DIR/"${FILE_LIST[@]}"
+git commit -m "Generating data files with signal values for testing"
+git push origin master	
